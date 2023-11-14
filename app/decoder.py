8e2f40b6fd71794f4e4671e64c521b7804730020
Vukasin Djuketic <v.djuketic@gmail.com>
2023-11-14 21:05:53 +0100

    removed console logs

--- app/decoder.py
+++ app/decoder.py
@@ -1,6 +1,4 @@
 def decode_bencode(bencoded_value):
-    print("decoding: ", bencoded_value)
-    
     while len(bencoded_value) > 0:
         if chr(bencoded_value[0]).isdigit():
             return decode_string(bencoded_value)
@@ -15,7 +13,6 @@ def decode_bencode(bencoded_value):
             raise NotImplementedError("Only strings, digits and lists are supported at the moment")
 
 def decode_string(bencoded_string):
-    print("decoding string: ", bencoded_string)
     colon_index = bencoded_string.find(b":")
     if colon_index == -1:
         raise ValueError("Invalid encoded value")
@@ -28,7 +25,6 @@ def decode_string(bencoded_string):
     return decoded_string, end_delimeter
 
 def decode_int(bencoded_int):
-    print("decoding int: ", bencoded_int)
     end_delimeter = bencoded_int.find(b"e")
     bencoded_int = bencoded_int[1:end_delimeter]
 
@@ -40,7 +36,6 @@ def decode_list(bencoded_list):
     decoded_list = []
     list_end_delimeter = 0
 
-    print("decoding list: ", bencoded_list)
     while chr(bencoded_list[0]) != "e":
         decoded_part, end_delimeter = decode_bencode(bencoded_list[1:])
         decoded_list.append(decoded_part)
