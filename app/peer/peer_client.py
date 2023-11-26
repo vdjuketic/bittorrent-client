f77e2f7826241c175063994a20371e7ba0285f20
Vukasin Djuketic <v.djuketic@gmail.com>
2023-11-26 13:45:48 +0100

    adjusted peer timeout

--- app/peer/peer_client.py
+++ app/peer/peer_client.py
@@ -127,7 +127,7 @@ class PeerClient:
         return message[8:]
 
     def receive_message(self) -> tuple[int, int, bytes]:
-        self.socket.settimeout(2)
+        self.socket.settimeout(7)
         length = int.from_bytes(self.socket.recv(4), "big")
         self.socket.settimeout(None)
 
